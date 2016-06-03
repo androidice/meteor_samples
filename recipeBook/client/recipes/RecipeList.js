c498610015fb73b99f4dbd72ecae65b715855284
ka185066 <kevinalviola@gmail.com>
2016-06-03 17:32:18 +0800

    Update
    update on edit form using session variables

--- recipeBook/client/recipes/RecipeList.js
+++ recipeBook/client/recipes/RecipeList.js
@@ -1,11 +1,17 @@
 Template.RecipeList.helpers({
-	updateRecipeId: function() {
-		return this._id;
-	}
+    updateRecipeId: function () {
+        return this._id;
+    }
 });
 
 Template.RecipeList.events({
-	'click .toggle-menu': function(){
-		Meteor.call("toggleMenuItem", this._id, this.inMenu)
-	}
+    'click .toggle-menu': function () {
+        Meteor.call("toggleMenuItem", this._id, this.inMenu);
+    },
+    'click .fa-trash': function () {
+    	Meteor.call("deleteRecipe", this._id);
+    },
+    'click .fa-pencil': function () {
+    	Session.set('editMode', !Session.get('editMode'));
+    }
 });
