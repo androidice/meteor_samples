d45dd5b0f8c937db78ec29765843c92a5d91ef5d
ka185066 <kevinalviola@gmail.com>
2016-06-03 17:57:29 +0800

    updates
    using using Reactive Var

--- recipeBook/client/recipes/RecipeList.js
+++ recipeBook/client/recipes/RecipeList.js
@@ -1,7 +1,14 @@
+Template.RecipeList.onCreated(function(){
+	this.editMode = new ReactiveVar(false);
+});
+
 Template.RecipeList.helpers({
     updateRecipeId: function () {
         return this._id;
-    }
+    },
+    editMode : function(){
+    	return Template.instance().editMode.get();
+    } 
 });
 
 Template.RecipeList.events({
@@ -11,7 +18,7 @@ Template.RecipeList.events({
     'click .fa-trash': function () {
     	Meteor.call("deleteRecipe", this._id);
     },
-    'click .fa-pencil': function () {
-    	Session.set('editMode', !Session.get('editMode'));
+    'click .fa-pencil': function (event, template) {
+    	template.editMode.set(!template.editMode.get());
     }
 });
